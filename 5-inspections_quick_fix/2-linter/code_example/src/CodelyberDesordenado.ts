9b8e8b7f3b39abe1fbcf6917986a1e47ca8de171
Rafa Gómez <rgomezcasas@gmail.com>
2023-05-08 18:49:31 +0200

    feat(6-1): add example

--- 5-inspections_quick_fix/2-linter/code_example/src/CodelyberDesordenado.ts
+++ 5-inspections_quick_fix/2-linter/code_example/src/CodelyberDesordenado.ts
@@ -1,3 +1,4 @@
+import { SomeClass } from "./another/folder/inside/here/SomeClass";
 import { Course, Product, User } from "./Classes";
 
 export class CodelyberDesordenado {
@@ -8,6 +9,7 @@ export class CodelyberDesordenado {
 		const course = new Course();
 		const user = new User();
 		const product = new Product();
+		const someClass = new SomeClass();
 
 		return `${course.name()} ${user.name()} ${product.name()}`;
 	}
