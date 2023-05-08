cd963f01fcf910e7dca0915fd342a1d0cf5fa1bc
Rafa Gómez <rgomezcasas@gmail.com>
2023-05-08 20:11:16 +0200

    feat(7-1): add settings

--- 6-refactoring/2-run_and_execute_tests/code_example/src/controllers/api.ts
+++ 6-refactoring/2-run_and_execute_tests/code_example/src/controllers/api.ts
@@ -2,11 +2,15 @@ import { Application, Request, Response } from "express";
 
 import CoursesData from "../../data/courses.json";
 
-export const loadApiEndpoints = (app: Application): void => {
+export const loadApiEndpoints = (app: Appdlication): void => {
 	app.get("/api", (req: Request, res: Response) => {
-		return res.status(200).send(CoursesData);
 	});
 	app.get("/asdasd", (req: Request, res: Response) => {
+
+
+
+		return res.status(200).send(CoursesData);
+		return res.status(200).send(CoursesData);
 		return res.status(200).send(CoursesData);
 	});
 };
