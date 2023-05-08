e14a333e9f15de9e98e47d332ff225f954de602e
Rafa Gómez <rgomezcasas@gmail.com>
2023-05-08 19:41:42 +0200

    feat(6-2): add example

---
+++ 6-refactoring/2-run_and_execute_tests/code_example/src/controllers/api.ts
@@ -0,0 +1,14 @@
+import { Application, Request, Response } from "express";
+
+import CoursesData from "../../data/courses.json";
+
+export const loadApiEndpoints = (app: Application): void => {
+	app.get("/api", (req: Request, res: Response) => {
+		return res.status(200).send(CoursesData);
+	});
+	app.get("/asdasd", (req: Request, res: Response) => {
+		return res.status(200).send(CoursesData);
+	});
+};
+
+export class Pepe {}
