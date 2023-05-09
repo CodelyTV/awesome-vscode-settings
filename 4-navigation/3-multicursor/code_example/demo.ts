c0eb3628937fe5445b020e9db3455886afddb58b
Rafa Gómez <rgomezcasas@gmail.com>
2023-05-09 10:13:36 +0200

    feat(4-3): add example

---
+++ 4-navigation/3-multicursor/code_example/demo.ts
@@ -0,0 +1,20 @@
+class User {
+  rename(): void {
+    return;
+  }
+
+  upgrade(): void {
+    return;
+  }
+}
+
+const user = new User()
+user.rename();
+user.upgrade();
+user.rename();
+user.rename();
+user.upgrade();
+user.rename();
+user.rename();
+user.upgrade();
+user.rename();
