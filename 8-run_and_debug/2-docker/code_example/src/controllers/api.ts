46940c7655d7f14f96dc907de99e8dc42cea98f2
Rafa Gómez <rgomezcasas@gmail.com>
2023-05-22 16:00:45 +0200

    feat(8-2): add docker code base

---
+++ 8-run_and_debug/2-docker/code_example/src/controllers/api.ts
@@ -0,0 +1,15 @@
+import { Application, Request, Response } from "express";
+
+import CoursesData from "../../data/courses.json";
+
+export const loadApiEndpoints = (app: Application): void => {
+	app.get("/api", (req: Request, res: Response) => {
+		return res.status(200).send(CoursesData);
+	});
+
+	app.get("/error", (req: Request, res: Response) => {
+		throw new Error("Ha petado");
+
+		return res.status(200).send(CoursesData);
+	});
+};
