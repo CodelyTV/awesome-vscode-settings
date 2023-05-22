5fc72ef57a727b9a09b7c4501172fdea66e46e9c
Rafa Gómez <rgomezcasas@gmail.com>
2023-05-22 11:25:57 +0200

    feat(8-1): add error example

--- 8-run_and_debug/1-debug/code_example/typescript-api/src/controllers/api.ts
+++ 8-run_and_debug/1-debug/code_example/typescript-api/src/controllers/api.ts
@@ -4,7 +4,13 @@ import CoursesData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
 	app.get("/api", (req: Request, res: Response) => {
-		// const data = CoursesData;
+		// const data = CoursesData
+
+		return res.status(200).send(CoursesData);
+	});
+
+	app.get("/error", (req: Request, res: Response) => {
+		throw new Error("Ha petado");
 
 		return res.status(200).send(CoursesData);
 	});
