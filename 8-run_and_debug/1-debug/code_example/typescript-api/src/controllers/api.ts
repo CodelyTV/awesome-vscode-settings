dc09c5330abd8498d67a5115c31cdb1a9bd71e62
Rafa Gómez <rgomezcasas@gmail.com>
2023-05-22 11:04:26 +0200

    feat(8-1): add api example

--- 8-run_and_debug/1-debug/code_example/typescript-api/src/controllers/api.ts
+++ 8-run_and_debug/1-debug/code_example/typescript-api/src/controllers/api.ts
@@ -4,6 +4,8 @@ import CoursesData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
 	app.get("/api", (req: Request, res: Response) => {
+		// const data = CoursesData;
+
 		return res.status(200).send(CoursesData);
 	});
 };
